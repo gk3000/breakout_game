768b8afc41fe9b5a60b962ee958008159c49ddcf
gaelleDcodeMoi <43601021+gaelleDcodeMoi@users.noreply.github.com>
2018-11-26 18:12:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -78,16 +78,20 @@ function drawBricks() {
     }
 }
 
-function collisionDetection(){
-	for(var c=0; c<brickColumnCount; c++){
-		for(var r=0; r<brickRowCount; r++){
-			var b = bricks[c][r];
-			if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight){
-				dy = -dy
-			}
-		}
-	}
+function collisionDetection() {
+    for(var c=0; c<brickColumnCount; c++) {
+        for(var r=0; r<brickRowCount; r++) {
+            var b = bricks[c][r];
+            if(b.status == 1) {
+                if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                }
+            }
+        }
+    }
 }
+
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
